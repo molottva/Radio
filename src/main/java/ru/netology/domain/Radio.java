09a905cd051d137c1390ebfb5da11e967e2ca305
molottva <molottva@list.ru>
2022-04-20 15:02:05 +0300

    Constructors added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,29 +1,33 @@
 package ru.netology.domain;
 
 public class Radio {
-    static final int maxStation = 9;
+    final int maxStation;
     static final int minStation = 0;
     private int currentStation;
-    static final int maxVolume = 10;
+    static final int maxVolume = 100;
     static final int minVolume = 0;
-    private int currentVolume;
+    private int currentVolume = 30;
+
+    public Radio() { //по умолчанию
+        this.maxStation = 9;
+    }
+
+    public Radio(int counterStation) { //изменения кол-ва станций
+        this.maxStation = counterStation - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation || currentStation > maxStation) {
+            return;
+        }
         this.currentStation = currentStation;
     }
 
-    public void setSelectStation(int selectStation) { //выбор номера станции напрямую
-        if (selectStation < minStation || selectStation > maxStation) {
-            return;
-        }
-        this.currentStation = selectStation;
-    }
-
-    public void setNextStation() { //след станция
+    public void nextStation() { //след станция
         if (currentStation == maxStation) {
             this.currentStation = minStation;
         } else {
@@ -31,7 +35,7 @@ public class Radio {
         }
     }
 
-    public void setPrevStation() { //пред станция
+    public void prevStation() { //пред станция
         if (currentStation == minStation) {
             this.currentStation = maxStation;
         } else {
@@ -44,17 +48,21 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
+            return;
+        } else {
+            this.currentVolume = currentVolume;
+        }
     }
 
-    public void setVolumeUp() { //повышение громкости
+    public void volumeUp() { //повышение громкости
         if (currentVolume == maxVolume) {
             return;
         }
         this.currentVolume = currentVolume + 1;
     }
 
-    public void setVolumeDown() { //понижение громкости
+    public void volumeDown() { //понижение громкости
         if (currentVolume == minVolume) {
             return;
         }
